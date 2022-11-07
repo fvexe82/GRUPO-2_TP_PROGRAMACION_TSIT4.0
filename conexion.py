e8479261d88a29f678b5d86e6ff5d491d05d385f
borianijc <borianijc@gmail.com>
2022-11-07 17:55:32 -0300

    Commit 07/11/2022

--- conexion.py
+++ conexion.py
@@ -72,24 +72,92 @@ class Registro_datos():
         self.conexion.commit()
         cursor.close()
 
+    # metodo para buscar genero
+    def buscar_interpretes(self):
+        cursor = self.conexion.cursor()
+        buscar_interprete = "SELECT ID_INTERPRETE,NOMBRE, APELLIDO FROM INTERPRETE" # ORDER BY NOMBRE ASC
+        cursor.execute(buscar_interprete)
+        interpretes_obtenidos = cursor.fetchall()        
+        lista_interpretes=[]
+        for x in interpretes_obtenidos:
+            lista_interpretes.append(str(x[0])+" - "+x[1]+" "+x[2])
+        cursor.close()
+        return (lista_interpretes)
+
+    # metodo para buscar id_interprete
+    def buscar_id_interprete(self,data):
+        cursor = self.conexion.cursor()
+        buscar_id_interp = "SELECT * FROM INTERPRETE WHERE ID_INTERPRETE = '{}'".format(data)
+        cursor.execute(buscar_id_interp)
+        id_interp_obtenidos = cursor.fetchall()
+        cursor.close()
+        return id_interp_obtenidos# [0]
+
+    # metodo para buscar genero
+    def buscar_genero(self):
+        cursor = self.conexion.cursor()
+        buscar_genero = "SELECT * FROM GENERO ORDER BY NOMBRE ASC"
+        cursor.execute(buscar_genero)
+        genero_obtenido = cursor.fetchall()
+        genero_obtenido = [x[1] for x in genero_obtenido]
+        cursor.close()
+        return genero_obtenido
+
+    # metodo para traer id de genero
+    def buscar_id_genero(self,data):
+        cursor = self.conexion.cursor()
+        buscar_id_genero = "SELECT * FROM GENERO WHERE NOMBRE = '{}'".format(data)
+        cursor.execute(buscar_id_genero)
+        id_genero_obtenido = cursor.fetchall()
+        cursor.close()
+        return id_genero_obtenido [0][0]       
+
+    # metodo para buscar formato
+    def buscar_formatos(self):
+        cursor = self.conexion.cursor()
+        buscar_formato = "SELECT * FROM FORMATO ORDER BY TIPO ASC"
+        cursor.execute(buscar_formato)
+        formatos_obtenidos = cursor.fetchall()
+        formatos_obtenidos = [x[1] for x in formatos_obtenidos]
+        cursor.close()
+        return formatos_obtenidos
+    
+    # metodo para traer id de formato
+    def buscar_id_formato(self,data):
+        cursor = self.conexion.cursor()
+        buscar_id_formato = "SELECT * FROM FORMATO WHERE TIPO = '{}'".format(data)
+        cursor.execute(buscar_id_formato)
+        id_formato_obtenidos = cursor.fetchall()
+        cursor.close()
+        return id_formato_obtenidos [0][0]
+
     # metodo para buscar discograficas
     def buscar_discografica(self):
         cursor = self.conexion.cursor()
-        buscar_discograficas = "SELECT * FROM DISCOGRAFICA"
+        buscar_discograficas = "SELECT * FROM DISCOGRAFICA ORDER BY NOMBRE ASC"
         cursor.execute(buscar_discograficas)
         discograficas_obtenidas = cursor.fetchall()
+        discograficas_obtenidas = [x[1] for x in discograficas_obtenidas]
         cursor.close()
         return discograficas_obtenidas
+    
+    # metodo para traer id de discografica
+    def buscar_id_discografica(self,data):
+        cursor = self.conexion.cursor()
+        buscar_id_disc = "SELECT * FROM DISCOGRAFICA WHERE NOMBRE = '{}'".format(data)
+        cursor.execute(buscar_id_disc)
+        id_disc_obtenidos = cursor.fetchall()
+        cursor.close()
+        return id_disc_obtenidos [0][0]
 
     # metodo para listado por genero
     def listado_genero (self):
         cursor = self.conexion.cursor()
         listar_por_genero ='''SELECT cod_album, album.nombre, interprete.nombre, interprete.apellido, genero.nombre, discografica.nombre,
-         precio, cantidad, formato.tipo FROM album, interprete, discografica,formato,genero 
+         precio,formato.tipo FROM album, interprete, discografica,formato,genero 
          WHERE album.id_interprete = interprete.id_interprete AND album.id_discografica = discografica.id_discografica
          AND album.id_formato = formato.id_formato AND album.id_genero = genero.id_genero ORDER By genero.nombre asc'''
         cursor.execute(listar_por_genero)
         registro_genero = cursor.fetchall()
         return registro_genero
-        
 
